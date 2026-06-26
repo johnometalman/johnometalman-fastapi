c8c606bf1d323d0eeec57330a6fee637396cd8a5
John J Meza <johnometalman@Mac-mini-de-John.local>
2026-06-26 10:03:20 -0500

    Added a middleware to see the request time for each endpoint and print it in the console

--- app/main.py
+++ app/main.py
@@ -1,7 +1,8 @@
 
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
 from db import  create_all_tables
 from .routers import customers, transactions, invoices, extras, plans
+import time
 
 
 app = FastAPI(lifespan=create_all_tables)
@@ -10,6 +11,16 @@ app.include_router(transactions.router)
 app.include_router(invoices.router)
 app.include_router(extras.router)
 app.include_router(plans.router)
+
+
+# Este es un middleware para ver los logs de las peticiones, debo importar Request
+@app.middleware("http") # Debo pasarle el http
+async def log_request_time(request: Request, call_next):
+    start_time = time.time() # Da un punto de partida
+    response = await call_next(request) # Ejecuta la petición
+    process_time = time.time() - start_time # Calcula el tiempo de procesamiento
+    print(f"Request {request.url} processed in {process_time:.4f} seconds") # Imprime el tiempo de procesamiento
+    return response
 
 
 
